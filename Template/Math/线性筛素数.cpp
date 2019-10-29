80bfb251f3024c395a3dbe17d39ee73dca759534
Echohat <fzulinxin@163.com>
2019-10-29 23:42:10 +0800

    Update 线性筛素数.cpp

--- Template/Math/线性筛素数.cpp
+++ Template/Math/线性筛素数.cpp
@@ -3,9 +3,7 @@ void sift_prime(bool notprime[],int N){
 	memset(notprime,false,sizeof(bool)*N);
 	notprime[0] = notprime[1] = 1;
 	for (int i = 2; i < N; ++i){
-		if (!notprime[i]){
-			prime.push_back(i);
-		}
+		if (!notprime[i]) prime.push_back(i);
 		for (int j = 0; i * prime[j] <= N && j < prime.size(); ++j){
 			notprime[i * prime[j]] = 1;
 			if (i % prime[j] == 0) break; //speed up linear time
